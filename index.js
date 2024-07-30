4c62447363e30b6a7cbf7c0f3d15f569ef2de9ad
Sabrina Clementino <sabrinasantos783@outlook.com>
2024-07-29 22:52:42 -0300

    Array de tarefas

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-const form = document.querySelector("#todo-form")
+const form = document.querySelector("#todo-list")
 const taskTitleInput = document.querySelector("#task-title-input")
-const todoListUl = document.querySelector("#todo-list")
+
+tasks = []
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+const taskTitle = taskTitleInput.value
+
+tasks.push({
+  title: taskTitle,
+  done: false,
+})
+
+localStorage.setItem("tasks", JSON.stringify(tasks))
+
+taskTitleInput.value = ""
+})
+
+//Peguei elementos para add eventos a eles, pegar seus valores, criar obj e coloca-lo no array e no localStorage, limpar input após atualizacao da pagina
