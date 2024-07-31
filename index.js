45c5dd34791d23bac27424bcee940958884975c9
Sabrina Clementino <sabrinasantos783@outlook.com>
2024-07-30 21:44:45 -0300

    Função para renderizar no HTML

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const form = document.querySelector("#todo-list");
+const form = document.querySelector("#todo-form");
 const taskTitleInput = document.querySelector("#task-title-input");
+const todoListUl = document.querySelector("#todo-list");
 
 let tasks = [];
+
+function renderTaskOnHTML(taskTitle, done = false) {
+  const li = document.createElement("li");
+  const input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+
+  const span = document.createElement("span");
+  span.textContent = taskTitle;
+
+  const button = document.createElement("button");
+  button.textContent = "Remover";
+
+  input.checked = done;
+
+  if (done) {
+    span.style.textDecoration = "line-through";
+  }
+
+  li.appendChild(input);
+  li.appendChild(span);
+  li.appendChild(button);
+
+  todoListUl.appendChild(li);
+}
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
